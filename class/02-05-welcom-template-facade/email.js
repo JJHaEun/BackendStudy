8d315928fbf80f12483e689d6c3bf7d9b49e82de
JJHaEun <yj05037@gmail.com>
2023-05-26 19:13:50 +0900

    이메일 템플릿 전송하기

--- class/02-05-welcom-template-facade/email.js
+++ class/02-05-welcom-template-facade/email.js
@@ -1,22 +1,28 @@
+import { getDate } from "../commons/getDate.js";
+
 export const checkValidationEmail = (email) => {
-  if (email.includes("@") === false) {
+  if (email.includes("@") === false || email === undefined) {
     console.log("이메일을 확인해주세요");
     return false;
   } else {
     return true;
   }
 };
-export const getWelcomeTemplate = ({ name, createdAt }) => {
-  const result = `
+export const getWelcomeTemplate = ({ name }) => {
+  const createdAt = new Date();
+  return `
           <html>
               <body>
                   <h1>${name}님 가입을 환영합니다!!!</h1>
                       <div>
                           <span>이름: ${name}</span>
-                          <span>가입일: ${createdAt}</span>
+                          <span>가입일: ${getDate(createdAt)}</span>
                       </div> 
               </body>
           <html>
           `;
-  console.log(result);
+};
+
+export const sendTemplateEmail = (email, myTemplate) => {
+  console.log(`${email}에 ${myTemplate}을 전송하였습니다`);
 };
--- class/02-05-welcom-template-facade/index.js
+++ class/02-05-welcom-template-facade/index.js
@@ -1,12 +1,38 @@
-import { checkValidationEmail, getWelcomeTemplate } from "./email.js";
+import {
+  checkValidationEmail,
+  getWelcomeTemplate,
+  sendTemplateEmail,
+} from "./email.js";
 
-const name = "유리";
-const createdAt = "2023-12-25";
+// const name = "유리";
+// const createdAt = "2023-12-25";
 
-const SendEmail = (email) => {
-  const isValid = checkValidationEmail(email);
+// const SendEmail = (email) => {
+//   const isValid = checkValidationEmail(email);
+//   if (isValid) {
+//     getWelcomeTemplate({ name, createdAt });
+//   }
+// };
+// SendEmail("123@123.com");
+
+//------------------------------------------------
+function createUser(user) {
+  // 1. 이메일이 정상인지 확인하기(1. 존재여부, 2."@"포함여부)
+  const isValid = checkValidationEmail(user.email);
   if (isValid) {
-    getWelcomeTemplate({ name, createdAt });
+    // 2. 가입환영 템플릿 만들기
+    const myTemplate = getWelcomeTemplate(user);
+
+    // 3. 이메일에 가입환영 템플릿 전송하기
+    sendTemplateEmail(user.email, myTemplate);
   }
+}
+
+const User = {
+  name: "짱구",
+  age: 6,
+  school: "떡잎초등학교",
+  email: "123@123.com",
 };
-SendEmail("123@123.com");
+
+createUser(User);
